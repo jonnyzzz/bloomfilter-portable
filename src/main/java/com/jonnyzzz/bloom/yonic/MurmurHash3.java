8b72cd2bf156b651c574917d01c9d8d2e1d4b163
Eugene Petrenko <me@jonnyzzz.com>
2020-07-08 22:42:40 +0200

    minor

--- src/main/java/com/jonnyzzz/bloom/yonic/MurmurHash3.java
+++ src/main/java/com/jonnyzzz/bloom/yonic/MurmurHash3.java
@@ -8,17 +8,11 @@ import org.jetbrains.annotations.NotNull;
  *  The MurmurHash3 algorithm was created by Austin Appleby and placed in the public domain.
  *  This java port was authored by Yonik Seeley and also placed into the public domain.
  *  The author hereby disclaims copyright to this source code.
- *  <p>
+ *  <br/>
  *  This produces exactly the same hash values as the final C++
  *  version of MurmurHash3 and is thus suitable for producing the same hash values across
  *  platforms.
- *  <p>
- *  The 32 bit x86 version of this hash should be the fastest variant for relatively short keys like ids.
- *  murmurhash3_x64_128 is a good choice for longer strings or if you need more than 32 bits of hash.
- *  <p>
- *  Note - The x86 and x64 versions do _not_ produce the same results, as the
- *  algorithms are optimized for their respective platforms.
- *  <p>
+ *  <br/>
  *  See http://github.com/yonik/java_util for future updates to this file.
  */
 public final class MurmurHash3 {
@@ -82,9 +76,6 @@ public final class MurmurHash3 {
             k2 *= c2; k2  = Long.rotateLeft(k2,33); k2 *= c1; h2 ^= k2;
             h2 = Long.rotateLeft(h2,31); h2 += h1; h2 = h2*5+0x38495ab5;
         }
-
-        //----------
-        // finalization
 
         h1 ^= len; h2 ^= len;
 

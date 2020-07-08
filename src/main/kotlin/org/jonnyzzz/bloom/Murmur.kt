8a4a98cafd5c7e2d4b2de7af44bb3b87bb081907
Eugene Petrenko <me@jonnyzzz.com>
2020-07-08 23:53:45 +0200

    make murmur3 inline function private

--- src/main/kotlin/org/jonnyzzz/bloom/Murmur.kt
+++ src/main/kotlin/org/jonnyzzz/bloom/Murmur.kt
@@ -13,23 +13,6 @@ package org.jonnyzzz.bloom
  * Note, this is simplified and patched version of the Murmur3_128 algorithm
  */
 object MurmurHash3 {
-    @PublishedApi
-    @Suppress("NOTHING_TO_INLINE")
-    internal inline fun rotateLeft(i: Long, distance: Int): Long = i shl distance or (i ushr -distance)
-
-    @PublishedApi
-    @Suppress("NOTHING_TO_INLINE")
-    internal inline fun fmix64(k: Long): Long {
-        @Suppress("NAME_SHADOWING")
-        var k = k
-        k = k xor (k ushr 33)
-        k *= -0xae502812aa7333L
-        k = k xor (k ushr 33)
-        k *= -0x3b314601e57a13adL
-        k = k xor (k ushr 33)
-        return k
-    }
-
     tailrec fun murmurhash3_x64_128(s: String, seed: Int = 0) : Pair<Long, Long> {
         val rem = s.length % 8 //we been 128 padding aka 4 chars / 16 bytes
         //align the string with \0 chars
@@ -57,53 +40,69 @@ object MurmurHash3 {
             buf[offset + 0].toLong().and(0xffL)
         }, ::Pair)
     }
+}
 
-    /** Returns the MurmurHash3_x64_128 hash*/
-    inline fun <R> murmurhash3_x64_128(seed: Int,
-                                       sizeInLongs: Int,
-                                       getLongLittleEndian: (longOffset: Int) -> Long,
-                                       handleResult: (Long, Long) -> R
-    ): R {
-        require(sizeInLongs % 2 == 0) { "the input must be properly padded to 2 longs / 128 bites" }
 
-        // The original algorithm does have a 32 bit unsigned seed.
-        // We have to mask to match the behavior of the unsigned types and prevent sign extension.
-        var h1 = seed.toLong() and 0x00000000FFFFFFFFL
-        var h2 = seed.toLong() and 0x00000000FFFFFFFFL
-        val c1 = -0x783c846eeebdac2bL
-        val c2 = 0x4cf5ad432745937fL
+@Suppress("NOTHING_TO_INLINE")
+private inline fun rotateLeft(i: Long, distance: Int): Long = i shl distance or (i ushr -distance)
 
-        if (sizeInLongs % 2 != 0) throw RuntimeException("The array must have size mod 16 == 0")
+@Suppress("NOTHING_TO_INLINE")
+private inline fun fmix64(k: Long): Long {
+    @Suppress("NAME_SHADOWING")
+    var k = k
+    k = k xor (k ushr 33)
+    k *= -0xae502812aa7333L
+    k = k xor (k ushr 33)
+    k *= -0x3b314601e57a13adL
+    k = k xor (k ushr 33)
+    return k
+}
 
-        var i = 0
-        while (i < sizeInLongs) {
-            var k1 = getLongLittleEndian(i++)
-            var k2 = getLongLittleEndian(i++)
-            k1 *= c1
-            k1 = rotateLeft(k1, 31)
-            k1 *= c2
-            h1 = h1 xor k1
-            h1 = rotateLeft(h1, 27)
-            h1 += h2
-            h1 = h1 * 5 + 0x52dce729
-            k2 *= c2
-            k2 = rotateLeft(k2, 33)
-            k2 *= c1
-            h2 = h2 xor k2
-            h2 = rotateLeft(h2, 31)
-            h2 += h1
-            h2 = h2 * 5 + 0x38495ab5
-        }
+/** Returns the MurmurHash3_x64_128 hash*/
+private inline fun <R> murmurhash3_x64_128(seed: Int,
+                                   sizeInLongs: Int,
+                                   getLongLittleEndian: (longOffset: Int) -> Long,
+                                   handleResult: (Long, Long) -> R
+): R {
+    require(sizeInLongs % 2 == 0) { "the input must be properly padded to 2 longs / 128 bites" }
 
-        h1 = h1 xor (sizeInLongs * 8).toLong()
-        h2 = h2 xor (sizeInLongs * 8).toLong()
+    // The original algorithm does have a 32 bit unsigned seed.
+    // We have to mask to match the behavior of the unsigned types and prevent sign extension.
+    var h1 = seed.toLong() and 0x00000000FFFFFFFFL
+    var h2 = seed.toLong() and 0x00000000FFFFFFFFL
+    val c1 = -0x783c846eeebdac2bL
+    val c2 = 0x4cf5ad432745937fL
+
+    if (sizeInLongs % 2 != 0) throw RuntimeException("The array must have size mod 16 == 0")
+
+    var i = 0
+    while (i < sizeInLongs) {
+        var k1 = getLongLittleEndian(i++)
+        var k2 = getLongLittleEndian(i++)
+        k1 *= c1
+        k1 = rotateLeft(k1, 31)
+        k1 *= c2
+        h1 = h1 xor k1
+        h1 = rotateLeft(h1, 27)
         h1 += h2
+        h1 = h1 * 5 + 0x52dce729
+        k2 *= c2
+        k2 = rotateLeft(k2, 33)
+        k2 *= c1
+        h2 = h2 xor k2
+        h2 = rotateLeft(h2, 31)
         h2 += h1
-        h1 = fmix64(h1)
-        h2 = fmix64(h2)
-        h1 += h2
-        h2 += h1
-
-        return handleResult(h1, h2)
+        h2 = h2 * 5 + 0x38495ab5
     }
+
+    h1 = h1 xor (sizeInLongs * 8).toLong()
+    h2 = h2 xor (sizeInLongs * 8).toLong()
+    h1 += h2
+    h2 += h1
+    h1 = fmix64(h1)
+    h2 = fmix64(h2)
+    h1 += h2
+    h2 += h1
+
+    return handleResult(h1, h2)
 }

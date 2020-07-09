83790e5a0fc66e087a00b089b29dd13f82c01c30
Eugene Petrenko <me@jonnyzzz.com>
2020-07-09 22:11:32 +0200

    add more tests (failing now, looks like a bug in the code)

--- src/test/kotlin/org/jonnyzzz/bloom/StringFiltersTest.kt
+++ src/test/kotlin/org/jonnyzzz/bloom/StringFiltersTest.kt
@@ -2,6 +2,7 @@ package org.jonnyzzz.bloom
 
 import org.junit.Assert
 import org.junit.Test
+import java.security.SecureRandom
 
 class StringFiltersTest {
     @Test
@@ -13,11 +14,32 @@ class StringFiltersTest {
     }
 
     @Test
-    fun testSimple() {
-        val expectedP = 0.95
-        val input = setOf("a", "b", "c")
-        val filter = BloomFilters.trainStringFilter(input, expectedP)
+    fun testSimple() = doExperiment(input = listOf("a", "b", "c"), testSet = StringPermutations.allStringsOfSize(3))
 
+    @Test
+    fun test_100() = doExperiment(inputSize = 100, testSetPermDeep = 3)
+
+    @Test
+    fun test_1000() = doExperiment(inputSize = 1000, testSetPermDeep = 3)
+
+    @Test
+    fun test_10000() = doExperiment(inputSize = 10000, testSetPermDeep = 3)
+
+    private fun doExperiment(expectedP: Double = 0.95,
+                             inputSize: Int,
+                             testSetPermDeep: Int
+    ) {
+        val testSet = StringPermutations.allStringsOfSize(testSetPermDeep)
+        val input = testSet.distinct().take(inputSize).toList().shuffled(SecureRandom())
+        require(input.size == inputSize) { "Too few elements in the testSet sequence"}
+        doExperiment(expectedP, input, testSet)
+    }
+
+    private fun doExperiment(expectedP: Double = 0.95,
+                             input: Collection<String>,
+                             testSet: Sequence<String>
+    ) {
+        val filter = BloomFilters.trainStringFilter(input, expectedP)
         for (s in input) {
             Assert.assertTrue("must contains - $s", filter.contains(s))
         }
@@ -25,16 +47,15 @@ class StringFiltersTest {
         var count = 0
         var error = 0
 
-        StringPermutations.allStringsOfSize(3).forEach {
+        testSet.forEach {
             if (it !in input) {
                 count++
                 if (filter.contains(it)) error++
             }
         }
 
-        val actualP = 1 - error.toDouble() / count
+        val actualP = 1.0 - error.toDouble() / count
         println("There are $error errors for $count tries: P(correctAnswer) = $actualP")
         Assert.assertTrue(actualP >= expectedP)
     }
 }
-

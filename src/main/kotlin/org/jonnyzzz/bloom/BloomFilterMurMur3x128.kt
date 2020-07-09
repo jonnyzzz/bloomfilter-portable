0f91bf42b43798e30a84adefaaedbafbe3e47ae7
Eugene Petrenko <me@jonnyzzz.com>
2020-07-09 23:04:20 +0200

    flip methods order

--- src/main/kotlin/org/jonnyzzz/bloom/BloomFilterMurMur3x128.kt
+++ src/main/kotlin/org/jonnyzzz/bloom/BloomFilterMurMur3x128.kt
@@ -40,21 +40,6 @@ internal object BloomFilterMurMur3x128 {
         TODO("Not implemented for the case where more than one mur3 hash is needed")
     }
 
-    private inline fun <reified T : Any> bloomFilterImpl(source: Set<T>,
-                                                         murmur: MurMur3HashFunction<T>,
-                                                         noinline updater: (Long, Long) -> Unit,
-                                                         noinline checker: (Long, Long) -> Boolean): BloomFilter<T> {
-        //fill in the data
-        source.forEach { murmur.hash(it, 0, updater) }
-
-        return object : BloomFilter<T> {
-            override fun contains(t: T?): Boolean {
-                if (t == null) return false
-                return murmur.hash(t, 0, checker)
-            }
-        }
-    }
-
     private inline fun <reified T : Any> bloomFilterBuilder_singleMur(source: Set<T>,
                                                                       numberOfBits: Int,
                                                                       numberOfHashFunctions: Int,
@@ -76,6 +61,21 @@ internal object BloomFilterMurMur3x128 {
         }
 
         return bloomFilterImpl(source, murmur, updater, checker)
+    }
+
+    private inline fun <reified T : Any> bloomFilterImpl(source: Set<T>,
+                                                         murmur: MurMur3HashFunction<T>,
+                                                         noinline updater: (Long, Long) -> Unit,
+                                                         noinline checker: (Long, Long) -> Boolean): BloomFilter<T> {
+        //fill in the data
+        source.forEach { murmur.hash(it, 0, updater) }
+
+        return object : BloomFilter<T> {
+            override fun contains(t: T?): Boolean {
+                if (t == null) return false
+                return murmur.hash(t, 0, checker)
+            }
+        }
     }
 
     /**
